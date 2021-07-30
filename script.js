99ae07ecc733f0001ff29ac5247dfe9cfabd1624
MaXOlimP <kirichukm@gmail.com>
2021-07-30 18:10:31 +0200

    last commit in friday

--- script.js
+++ script.js
@@ -11,4 +11,19 @@ const obj = {
     isMarried: false
 };
 
+
 console.log(obj.name);
+
+const category = 'toys';
+
+console.log(`http://someurl.com/${category}/5`);
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+
+console.log(--decr);

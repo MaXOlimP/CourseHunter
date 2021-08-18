a4facd04865c5ce77d3951b3a1280aced3f8e441
MaXOlimP <kirichukm@gmail.com>
2021-08-18 17:06:56 +0200

    second task for course

--- script.js
+++ script.js
@@ -13,16 +13,38 @@ let personalMovieDB = {
     private: false
 };
 
-const a = prompt("One of the last see a films?"),
-    b = prompt("How you rate this?"),
-    c = prompt("One of the last see a films?"),
-    d = prompt("How you rate this?");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+//     c = prompt("One of the last see a films?"),
+//     d = prompt("How you rate this?");
 
-function newCheck(){
-    let a = 10;
-    const b = 100;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-};
+for(let i = 0; i < 2; i++){
+    const a = prompt("One of the last see a films?"),
+        b = prompt("How you rate this?");
+
+        if (a != null && b != null && a != '' && b != '' && a.length < 50){
+    
+            console.log('done');
+        } else{
+            console.log('errors');
+            i--;
+        }
+
+        personalMovieDB.movies[a]=b;
+}
+
+if(personalMovieDB.count < 10){
+    console.log("Просмотрено довольно мало фильмов");
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log("Вы классический зритель");
+} else if(personalMovieDB.count >= 30){
+    console.log("Вы киноман");
+} else{
+    console.log("Error");
+}
+
+
+
+console.log(personalMovieDB);
